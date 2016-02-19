4d9aa27bafcfbb9c215d56c119152fd1e56e64cd
Kpatena <kamilpatena1221@gmail.com>
2016-02-18 23:04:18 -0800

    A Simple Pedometer

--- arcticrun/arcticrun/PedometerViewController.swift
+++ arcticrun/arcticrun/PedometerViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import CoreMotion
 
 class PedometerViewController: UIViewController {
     
    
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    @IBOutlet weak var stepsText: UILabel!
+    let pedoMeter = CMPedometer()
+    var timer = NSTimer()
+    var rightNow:NSDate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +30,26 @@ class PedometerViewController: UIViewController {
         }
     }
     
+    //Set the date to start from now and start running step function in intervals
+    @IBAction func start(sender : AnyObject) {
+        rightNow = NSDate()
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.75, target: self, selector: Selector("updateCounting"), userInfo: nil, repeats: true)
+    }
+    
+    //Count the number of steps
+    func updateCounting() {
+        if(CMPedometer.isStepCountingAvailable()){
+            self.pedoMeter.queryPedometerDataFromDate(rightNow!, toDate: NSDate()) { (data : CMPedometerData?, error) -> Void in
+                print(data)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if(error == nil){
+                        self.stepsText.text = "\(data!.numberOfSteps)"
+                    }
+                })
+                
+            }
+        }
+    }
     /*
     // MARK: Navigation -
     
